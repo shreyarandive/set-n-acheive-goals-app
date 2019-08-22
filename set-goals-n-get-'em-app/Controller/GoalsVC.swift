0f7ff6f79ced9ba80fc9f6a957df201e8307b758
Shreya Randive <shreyarandive28@gmail.com>
2019-08-22 18:13:11 -0400

    Initial commit

---
+++ set-goals-n-get-'em-app/Controller/GoalsVC.swift
@@ -0,0 +1,20 @@
+//
+//  GoalsVC.swift
+//  set-goals-n-get-'em-app
+//
+//  Created by Shreya Randive on 8/22/19.
+//  Copyright © 2019 Shreya Randive. All rights reserved.
+//
+
+import UIKit
+
+class GoalsVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
